bb7389ba800f52e24cc72b5dcac4ee9488bc482f
Asassine20 <122495785+Asassine20@users.noreply.github.com>
2023-11-18 17:33:49 -0500

    Added registration form and login page

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const path = require('path');
 const server = express();
 const db = require('./db');
 const port = 3001;
+const publicDirectory = path.join(__dirname, './public')
+server.use(express.static(publicDirectory));
+
+server.set('view engine', 'hbs');
 
 server.get('/', (req, res) => {
-    res.send('Hello');
+    res.render("index");
 });
+
+server.get('/register', (req, res) => {
+    res.render("register");
+});
+
 
 server.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
