a817b0e1d9802c8b22f2dc70e3505817ea60d279
Asassine20 <122495785+Asassine20@users.noreply.github.com>
2024-03-25 21:40:25 -0400

    Edited the search query to make it much faster

--- next-frontend/src/pages/api/search.js
+++ next-frontend/src/pages/api/search.js
@@ -1,21 +1,32 @@
-// pages/api/search.js
 const { query } = require('@/db'); // Adjust the import path as necessary
 
 export default async function handler(req, res) {
     const { cardName, page = '1', limit = '24' } = req.query;
-    // Ensure page and limit are integers
     const pageNum = parseInt(page, 10);
     const limitNum = parseInt(limit, 10);
     const offset = (pageNum - 1) * limitNum;
 
+    // Base SQL and values array
+    let sql = `SELECT * FROM Card`;
+    let whereConditions = [];
+    let values = [];
+
+    // Check if there's a cardName search term
+    if (cardName) {
+        whereConditions.push("CardName LIKE ?");
+        values.push(`${cardName.trim()}%`); // Using '%' at the end for prefix matching
+    }
+
+    // If there are any conditions, append them to the query
+    if (whereConditions.length > 0) {
+        sql += ` WHERE ` + whereConditions.join(' AND ');
+    }
+
+    // Append LIMIT and OFFSET for pagination
+    sql += ` LIMIT ? OFFSET ?`;
+    values.push(limitNum, offset);
+
     try {
-        const sql = `
-            SELECT * FROM Card
-            WHERE CardName like ?
-            LIMIT ? OFFSET ?
-        `;
-        // Use the query function from your db module
-        const values = [`%${cardName}%`, limitNum, offset];
         const rows = await query(sql, values);
         res.status(200).json(rows);
     } catch (error) {
