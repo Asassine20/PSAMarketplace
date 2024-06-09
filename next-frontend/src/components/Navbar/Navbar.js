fa17792bcfe5ca7b6455251f2cd262d7cbf77438
Asassine20 <122495785+Asassine20@users.noreply.github.com>
2024-06-08 20:37:42 -0500

    Made big improvements to navbar and nav links. added popular sets and recent sets to navlinks inside of a mini panel. added a dropdown caret icon for better ui.

--- next-frontend/src/components/Navbar/Navbar.js
+++ next-frontend/src/components/Navbar/Navbar.js
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { IoCartOutline } from 'react-icons/io5';
 import { FaRegUser } from 'react-icons/fa';
+import { FaCaretDown } from 'react-icons/fa';
 import { useCart } from '../Cart/CartProvider';
 
 const Navbar = () => {
@@ -18,9 +19,12 @@ const Navbar = () => {
   const [mounted, setMounted] = useState(false);
   const [activeSport, setActiveSport] = useState(null);
   const [recentCardSets, setRecentCardSets] = useState([]);
+  const [popularCardSets, setPopularCardSets] = useState({});
   const router = useRouter();
   const { cart } = useCart();
   const sidePanelRef = useRef(null);
+  const miniPanelRef = useRef(null);
+  const activeSportIndex = useRef(null);
 
   useEffect(() => {
     const checkLoginStatus = () => {
@@ -104,9 +108,12 @@ const Navbar = () => {
       if (sidePanelRef.current && !sidePanelRef.current.contains(event.target)) {
         setIsSidePanelOpen(false);
       }
+      if (miniPanelRef.current && !miniPanelRef.current.contains(event.target)) {
+        setActiveSport(null);
+      }
     };
 
-    if (isSidePanelOpen) {
+    if (isSidePanelOpen || activeSport) {
       document.addEventListener('mousedown', handleClickOutside);
     } else {
       document.removeEventListener('mousedown', handleClickOutside);
@@ -115,9 +122,9 @@ const Navbar = () => {
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [isSidePanelOpen]);
+  }, [isSidePanelOpen, activeSport]);
 
-  const handleNavLinkClick = (sport) => {
+  const handleNavLinkClick = (sport, index) => {
     if (activeSport === sport) {
       setActiveSport(null);
     } else {
@@ -126,6 +133,39 @@ const Navbar = () => {
         .then(response => response.json())
         .then(data => setRecentCardSets(data))
         .catch(error => console.error('Error fetching recent card sets:', error));
+
+      // Set popular sets for each sport
+      const popularSetsData = {
+        'Football': ['Prizm', 'Select', 'Optic', 'Mosaic', 'Donruss', 'Prestige', 'Certified', 'Score', 'Spectra', 'Rookies and Stars'],
+        'Baseball': ['Topps', 'Bowman', 'Bowman Chrome', 'Topps Chrome', 'Topps Heritage', 'Donruss', 'Select', 'Upper Deck', 'Mosaic', 'Stadium Club'],
+        'Basketball': ['Hoops', 'Panini Prizm', 'Select', 'Mosaic', 'Optic', 'Donruss', 'Hoops Winter', 'Chronicles', 'Totally Ceritifed', 'Prestige'],
+        'Hockey': ['O-Pee-Chee', 'O-Pee-Chee Retro', 'Upper Deck', 'Upper Deck Exclusives', 'Parkhurst', 'Artifacts', 'Score', 'MVP', 'Black Diamond', 'Upper Deck Canvas'],
+        'Pokemon (English)': ['Evolving Skies', 'Fusion Strike', 'Crown Zenith', 'Base Set', 'Paldea Evolved', '151', 'Astral Radiance', 'Lost Origin', 'Obsidian Flames', 'Paldean Fates'],
+        'Pokemon (Japan)': ['Pokemon 151', 'Shiny Treasures ex', 'VSTAR Universe', 'VMAX Climax', 'Blue Sky Stream', 'Eevee Heroes', 'Shiny Star V', 'Clay Burst', 'Night Wanderer', 'Crimson Haze'],
+      };
+      setPopularCardSets(popularSetsData[sport] || []);
+
+      // Store the index of the active sport
+      activeSportIndex.current = index;
+    }
+  };
+
+  useEffect(() => {
+    if (activeSport !== null && miniPanelRef.current) {
+      const index = activeSportIndex.current;
+      const panelPosition = calculatePanelPosition(index, sports.length);
+      miniPanelRef.current.style.left = panelPosition.left;
+      miniPanelRef.current.style.right = panelPosition.right;
+    }
+  }, [activeSport]);
+
+  const calculatePanelPosition = (index, total) => {
+    if (index < 3) {
+      return { left: '0', right: 'auto' }; // Left-aligned
+    } else if (index >= total - 3) {
+      return { left: 'auto', right: '0' }; // Right-aligned
+    } else {
+      return { left: '0', right: 'auto' }; // Default
     }
   };
 
@@ -214,12 +254,13 @@ const Navbar = () => {
             >
               <div
                 className={styles.navLink}
-                onClick={() => handleNavLinkClick(sport.Sport)}
+                onClick={() => handleNavLinkClick(sport.Sport, index)}
               >
                 {sport.Sport}
+                <FaCaretDown className={styles.caretIcon} />
               </div>
               {activeSport === sport.Sport && (
-                <div className={styles.miniPanel}>
+                <div className={styles.miniPanel} ref={miniPanelRef}>
                   <div className={styles.miniPanelHeader}>
                     <span className={styles.miniPanelSport}><b>{sport.Sport}</b></span>
                     <Link
@@ -237,28 +278,56 @@ const Navbar = () => {
                       <span className={styles.shopAllButton}>Shop All</span>
                     </Link>
                   </div>
-                  <div className={styles.miniPanelTitle}>Recent Sets</div>
-                  <ul className={styles.miniPanelList}>
-                    {recentCardSets.map((cardSet, idx) => (
-                      <li key={idx}>
-                        <Link
-                          href={{
-                            pathname: '/search',
-                            query: {
-                              cardName: '',
-                              page: '1',
-                              inStock: 'true',
-                              sports: sport.Sport,
-                              cardSets: cardSet.CardSet
-                            }
-                          }}
-                          passHref
-                        >
-                          <span className={styles.miniPanelItem}>{cardSet.CardSet}</span>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
+                  <div className={styles.panelContent}>
+                    <div>
+                      <div className={styles.miniPanelTitle}>Recent Sets</div>
+                      <ul className={styles.miniPanelList}>
+                        {recentCardSets.map((cardSet, idx) => (
+                          <li key={idx}>
+                            <Link
+                              href={{
+                                pathname: '/search',
+                                query: {
+                                  cardName: '',
+                                  page: '1',
+                                  inStock: 'true',
+                                  sports: sport.Sport,
+                                  cardSets: cardSet.CardSet
+                                }
+                              }}
+                              passHref
+                            >
+                              <span className={styles.miniPanelItem}>{cardSet.CardSet}</span>
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                    <div className={styles.popularSetsContainer}>
+                      <div className={styles.miniPanelTitle}>Popular Sets</div>
+                      <ul className={styles.miniPanelList}>
+                        {popularCardSets.map((cardSet, idx) => (
+                          <li key={idx}>
+                            <Link
+                              href={{
+                                pathname: '/search',
+                                query: {
+                                  cardName: '',
+                                  page: '1',
+                                  inStock: 'true',
+                                  sports: sport.Sport,
+                                  cardSets: cardSet
+                                }
+                              }}
+                              passHref
+                            >
+                              <span className={styles.miniPanelItem}>{cardSet}</span>
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
                 </div>
               )}
             </div>
